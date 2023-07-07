5405e819d61a154cce0b6d12db492b1064010300
Tarek Rahman <99980505+tarekrahamn@users.noreply.github.com>
2023-07-07 13:27:15 +0600

    Add files via upload

---
+++ The_Vowel_Matrix.cpp
@@ -0,0 +1,84 @@
+/*
+ 
+               Starting with the name of almighty ALLAH 
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~**
+            In the name of Allah, Most Gracious, Most Merciful             
+                   লা ইলাহা ইল্লাল্লাহু মুহাম্মাদুর রাসুলুল্লাহ 
+                        Author:  tarek200 
+                        Created: 14.06.2023 
+                Practice is the only shortcut to improve  
+**/
+#include <bits/stdc++.h>
+#define   Tarek           ios::sync_with_stdio(false);cin.tie(0);
+#define   ll              long long int
+#define   ull             unsigned long long int
+#define   ld              long double
+#define   vi              vector<int>
+#define   vll             vector<ll>
+#define   vc              vector<char>
+#define   vs              vector<string>
+#define   test            ll t;cin >> t;while(t--)
+#define   pb              push_back
+#define   pp              pop_back
+#define   sz              size()
+#define   ff              first
+#define   ss              second
+#define   YES             cout<<"YES"<<endl;
+#define   NO              cout<<"NO"<<endl;
+#define   exit            return ;
+#define   next            continue;
+#define   B               break;
+#define   checkmate       return 0;
+#define   nl              "\n"
+#define   gcd(a,b)        __gcd(a,b)
+#define   lcm(a,b)        (a/gcd(a,b))*b
+#define   mod             1000000007
+#define   PI              3.14159265358979323846
+#define   all(x)         (x).begin(), (x).end()
+#define   des(x)         (x).rbegin(), (x).rend()
+#define   FOREACH(it, l)  for (auto it = l.begin(); it != l.end(); it++)
+#define   cinv(v)         for(auto &it:v)cin>>it;
+#define   FOR(i,a,b)      for(ll i=a;i<b;i++)
+#define   LOOP(i,a,b)     for(ll j=a;j<b;j++)
+#define   LOOP2(i,a,b)    for(ll i=a;i<=b;i++)
+#define   ROF(i, a, b)    for (ll i = a; i >b; i--)
+const long long int N     = (ll) 1e7+1;
+const long long int M     = (ll) 1e9+5;
+using namespace std;
+void run() {
+    ll n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    ll ans = 1;
+    ll c = 0;
+    bool flag = false;
+    ll c2 = 0;
+    bool flag2 = false;
+    FOR(i,0,n) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ) {
+            if ( flag=true && flag2) 
+                c2++;
+                ans = (ans * c2) % mod;
+            c++;
+            flag = true;
+            flag2 = true;
+            c2 = 0;
+        } 
+        if (!flag) {
+            c2++;
+        }
+        if (c == k) {
+            flag = false;
+            c = 0;
+        }
+    }
+    cout << ans << nl;
+}
+int main(){
+    Tarek
+    test{
+        run();
+    }
+    checkmate
+}
